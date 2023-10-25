bcd749b9ac63c91bd32bd64e7e809d95f90c6388
NKSHub <nskumar39345@gmail.com>
2023-10-25 22:36:43 +0530

    First Commit

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -3,9 +3,11 @@ package demo;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
 //Selenium Imports
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -28,16 +30,31 @@ public class TestCases {
     {
         System.out.println("End Test: TestCases");
         driver.close();
-        driver.quit();
+       driver.quit();
 
     }
 
     
-    public  void testCase01(){
+    public  void testCaseAmazonSearh01() throws InterruptedException{
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        WebElement searchbar= driver.findElement(By.id("APjFqb"));
+        searchbar.click();
+       // searchbar.click();
+       Thread.sleep(1000);
+
+        searchbar.sendKeys("Amazon");
+        Thread.sleep(1000);
+        searchbar.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        WebElement text =driver.findElement(By.xpath("//span[text()='Amazon.in']"));
+        System.out.println(text.getText());
+        text.isDisplayed();
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        System.out.println("end Test case: testCase01");
     }
+
+    
 
 
 }
