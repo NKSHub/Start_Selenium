919ad5dc5fdb8547b8fa42b8e3f79eaf00501265
NKSHub <nskumar39345@gmail.com>
2023-11-26 10:01:25 +0530

    Six Commit

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -191,6 +191,62 @@ public class TestCases {
             }
     
 
+            public  void testCasePostLinkedIN() throws InterruptedException{
+                System.out.println("Start Test case: testCase01");
+                //Navigate to linkedin URL "https://in.linkedin.com/"
+                driver.get("https://in.linkedin.com/");
+                // Click on "Sign in" link Using Locator "Partial Link Text" Sign in | click()
+                Thread.sleep(3000);
+                driver.findElement((By.partialLinkText("Sign in"))).click();
+                Thread.sleep(3000);
+                // In the "Sign in" page, enter username Using Locator "ID" username | sendkeys("phanikumar")
+                driver.findElement(By.id("username")).sendKeys("phani.cherukuri@gmail.com");
+                Thread.sleep(3000);
+                // In the "Sign in" page, enter password Using Locator "ID" password | sendkeys("**************")
+                driver.findElement(By.id("password")).sendKeys("********");
+                Thread.sleep(3000);
+                // Click on "Sign in" button Using Locator "XPath" //button[text()='Sign in'] | click()
+                driver.findElement(By.xpath("//button[text()='Sign in']")).click();
+                Thread.sleep(3000);
+                // Get the count of people who viewed your profile Using Locator "XPath" //span[contains(text(),'viewed')]/ancestor::div/div/span/strong | gettext()
+                System.out.println("Count of profile views: " + driver.findElement(By.xpath("//span[contains(text(),'viewed')]/ancestor::div/div/span/strong")).getText());
+                Thread.sleep(3000);
+                // Get the count of impressions of your post. Using Locator "XPath" //span[contains(text(),'Impressions')]/ancestor::div/div/span/strong | gettext()
+                System.out.println("Count of post impressions: " + driver.findElement(By.xpath("//span[contains(text(),'Impressions')]/ancestor::div/div/span/strong")).getText());
+                Thread.sleep(3000);
+                // Click on Start a post Using Locator "XPath" //button//span[text()='Start a post'] | click()
+                driver.findElement(By.xpath("//button//span[text()='Start a post']")).click();
+                Thread.sleep(3000);
+                // Click on the options to select to whom to be posted Using Locator "XPath" //span[contains(text(),'Phani')] | click()
+                driver.findElement(By.xpath("//span[contains(text(),'Phani')]")).click();
+                Thread.sleep(3000);
+                // Select the option "CONNECTIONS_ONLY' Using Locator "ID" CONNECTIONS_ONLY | click()
+                driver.findElement(By.id("CONNECTIONS_ONLY")).click();
+                Thread.sleep(3000);
+                // Click on the Done button. Using Locator "XPath" //button//span[text()='Done'] | click()
+                driver.findElement(By.xpath("//button//span[text()='Done']")).click();
+                Thread.sleep(3000);
+                // Create a post Using Locator "XPath" //div[@role='textbox'] | sendkeys("hello")
+                driver.findElement(By.xpath("//div[@role='textbox']")).sendKeys("hello");
+                Thread.sleep(3000);
+                // Click on post button. Using Locator "XPath" //button//span[text()='Post'] | click()
+                driver.findElement(By.xpath("//button//span[text()='Post']")).click();
+                Thread.sleep(3000);
+                // Click on View Post alert. Using Locator "XPath" //a[text()='View post'] | click()
+                driver.findElement(By.xpath("//a[text()='View post']")).click();
+                Thread.sleep(3000);
+                //
+                List<WebElement> imgList = driver.findElements(By.xpath("//img[contains(@alt,'Phani')]"));
+                imgList.get(0).click();
+                //Declare and initialise a fluent wait
+                //WebDriverWait wait = new WebDriverWait(driver,10);
+                Thread.sleep(3000);
+                //wait.until(ExpectedConditions.elementToBeClickable((By.cssSelector("a[href*='logout']")))).click();
+                driver.findElement(By.cssSelector("a[href*='logout']")).click();
+                Thread.sleep(3000);
+                System.out.println("end Test case: testCase01");
+            }
+    
    
 
 }
